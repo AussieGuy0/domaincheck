236a669821c66b46f54abfaec97188941862a8a7
AussieGuy0 <anthony.bruno196@gmail.com>
2017-10-08 11:07:42 +1030

    Add styling to index

--- server.js
+++ server.js
@@ -1,29 +1,34 @@
-const express = require('express')
-const app = express()
-const domainService = require('./service/domain')
-const View = require('./view/views')
+const express = require('express');
+const app = express();
+const domainService = require('./service/domain');
+const View = require('./view/views');
 
-const apiUrl = '/api/v1/'
-const encoding = 'utf-8'
+const apiUrl = '/api/v1/';
+const encoding = 'utf-8';
 
-app.get(`${apiUrl}:domain`, (req,res) => {
+app.get(`${apiUrl}:domain`, (req, res) => {
     const domainUrl = req.params.domain;
-    const domainCheck = domainService.checkDomain(domainUrl, (data) => res.end(data, encoding));
+    domainService.checkDomain(domainUrl, (data) => res.end(data, encoding));
 });
 
 /**
  * Checks the domain given in the query parameter 'domain' and returns a HTML view of the result.
  */
 app.get('/checkDomain', (req, res) => {
-    const domainUrl = req.query.domain;    
-    domainService.checkDomain(domainUrl, (whois) => {
-        if (whois.found) {
-            var view = new View("takenDomain", whois);
-            res.end(view.render());
-        } else {
-            res.end(JSON.stringify(whois), encoding);
-        }
-    });
+    const domainUrl = req.query.domain;
+    if (domainUrl.length === 0) {
+        res.redirect("/");
+    } else {
+        domainService.checkDomain(domainUrl, (whois) => {
+            if (whois.found) {
+                let view = new View("takenDomain", whois);
+                res.end(view.render());
+            } else {
+                res.end(JSON.stringify(whois), encoding);
+            }
+        });
+
+    }
 });
 
 /**
@@ -40,16 +45,14 @@ app.get('/subscribe', (req, res) => {
 app.use(express.static("public"));
 
 module.exports = {
-    listen: function(port) {
+    listen: function (port) {
         if (this.server != null) {
             this.close();
         }
         this.server = app.listen(port, () => console.log(`App running on port ${port}`));
     },
-    close: function() {
+    close: function () {
         this.server.close(() => console.log("Server closed!"));
         this.server = null;
     }
-}
-    
-
+};
